a8ebe5e1c2d98c5374ea3289521d7d42931e63d2
anilsinghn <76898153+anilsinghn@users.noreply.github.com>
2021-01-03 14:20:38 +0530

    Add files via upload

---
+++ email_send.py
@@ -0,0 +1,16 @@
+import smtplib
+from email.message import EmailMessage
+
+email = EmailMessage()
+email['from']= 'anil singh'
+email['to']= 'arun.sinngh@gmail.com'
+email['subject']= 'you won 1M dollars!'
+
+email.set_content('i am a scammer')
+
+with smtplib.SMTP(host='smtp.gmail.com',port=587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('blazegaming560@gmail.com','Myattitude@1')
+    smtp.send_message(email)
+    print("all good boss")
